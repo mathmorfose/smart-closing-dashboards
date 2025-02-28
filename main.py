012a7540d2cf82f747f77f893ba07a28a76df5bd
MATHEUS ARAUJO MOREIRA <99842570@ab-inbev.com>
2025-02-28 11:09:37 -0300

    added upload file

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import pygwalker as pyg
 from pygwalker.api.streamlit import StreamlitRenderer
 import pandas as pd
 import streamlit as st
@@ -10,8 +9,6 @@ st.set_page_config(
 )
 
 st.title("Build your own Smart Closing Dashboard")
-# pyg_html = pyg.walk(df, retur_html=True)
-# components.html(pyg_html, height=10000)
 
 # Upload button
 uploaded_file = st.file_uploader("Choose an Excel file", type=["xlsx", "xls"])
@@ -19,7 +16,6 @@ uploaded_file = st.file_uploader("Choose an Excel file", type=["xlsx", "xls"])
 # Process the file if uploaded
 if uploaded_file is not None:
     df = pd.read_excel(uploaded_file)
-    st.write("Preview of the uploaded file:")
 
     pyg_app = StreamlitRenderer(df)
     
